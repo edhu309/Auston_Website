06754c151a8fbf4fc542def0fb30a5711fabaca4
ArshaShaji3099 <arshashajiii65@gmail.com>
2026-09-16 21:30:43 +0530

    modify backend

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -148,3 +148,13 @@ CORS_ALLOWED_ORIGINS = [
 
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
+    "DEFAULT_PERMISSION_CLASSES": (
+        "rest_framework.permissions.AllowAny",
+    ),
+}
